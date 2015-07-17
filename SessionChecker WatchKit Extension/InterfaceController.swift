81de1f8edcb3720008cd24bb3e229f89c783d348
skybolt <skybolt@gmail.com>
2015-07-17 13:18:52 -0700

    Initial Commit

---
+++ SessionChecker WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  SessionChecker WatchKit Extension
+//
+//  Created by hp on 7/17/15.
+//  Copyright © 2015 klebeian. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ SessionChecker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SessionChecker
+//
+//  Created by hp on 7/17/15.
+//  Copyright © 2015 klebeian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
